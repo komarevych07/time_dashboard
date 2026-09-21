02bb11de55cc642ceda71f41b58451e97eca9e28
komarevych07 <sashakomua@gmail.com>
2026-09-21 17:28:35 +0300

    increase from 100 to 200

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -147,7 +147,7 @@ interface AccessibleResource {
   avatarUrl: string;
 }
 
-const DEFAULT_MAX_RESULTS = 100;
+const DEFAULT_MAX_RESULTS = 200;
 const ATLASSIAN_TOKEN_URL = 'https://auth.atlassian.com/oauth/token';
 const ATLASSIAN_RESOURCES_URL = 'https://api.atlassian.com/oauth/token/accessible-resources';
 
